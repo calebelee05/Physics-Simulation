51659c15fd9a5d0351387a4c7da65ae3dbe17847
calebelee05 <79616504+calebelee05@users.noreply.github.com>
2023-04-20 14:26:53 +0200

    Create Simulate.py

---
+++ Simulate.py
@@ -0,0 +1,15 @@
+# Simulate the movement of the set of points
+# timestep of (dt) seconds, updated (steps) times
+def simulate(points, dt=0.1, steps=1000):
+  n = len(points)
+  Coordinates = [[] for j in range(n)]
+  for _ in range(steps):
+    for i, point in enumerate(points):
+      OtherPoints = points.copy()
+      del OtherPoints[i]
+      F = point.NetForce(OtherPoints)
+      point.update(F, dt)
+      Coordinates[i].append(point.coordinate)
+  
+  # Return the history of the position of each points
+  return Coordinates
