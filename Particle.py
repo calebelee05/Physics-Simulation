c5edff4ca5209325cc449c865428639675669f64
calebelee05 <79616504+calebelee05@users.noreply.github.com>
2023-04-21 10:44:11 +0200

    Update and rename PointMass.py to Particle.py

--- Particle.py
+++ Particle.py
@@ -1,15 +1,19 @@
 import numpy as np
 
 G = 6.6743e-11
+k = 8.9876e9
 
 class Point:
-  def __init__(self, mass, coordinate, dim=2):
-    self.dim = dim                                                          # N-Dimension
+  def __init__(self, coordinate, mass=1.0, charge=0.0, dim=2, fixed=False):
+    self.dim = dim                                                          # N-dimensions
     self.mass = mass                                                        # scalar mass
+    self.coulomb = charge * 1.602e-19                                       # scalar charge in coulombs
     self.coordinate = np.array(coordinate).astype(float)                    # coordinate vector
     self.velocity = np.array([0.0 for n in range(dim)]).astype(float)       # velocity vector
     self.ConstantForce = np.array([0.0 for n in range(dim)]).astype(float)  # constant force vector
+    self.netforce = self.ConstantForce                                      # Net force vector
     self.dt = 0.1                                                           # timestep for updating velocity and displacement
+    self.fixed = fixed                                                      # whether the point is fixed in place
   
   # Calculate the displacement vector from current position with object 2
   def Displacement(self, other):
@@ -27,7 +31,7 @@ class Point:
     return r
   
   # Calculate the graviational force
-  def Force(self, other):
+  def GravitationalForce(self, other):
     r = self.Distance(other)
     M = other.mass
     _F_ = G * self.mass * M / r**2
@@ -36,18 +40,27 @@ class Point:
     F = direction * _F_
     return F
   
-  # Calculate the total net force
-  def NetForce(self, others=None):
-    F = np.array([0.0 for n in range(self.dim)])
-    F += self.ConstantForce
-    if others:
-      for other in others:
-        F += self.Force(other)
+  # Calculate the electrostatic force
+  def ElectrostaticForce(self,other):
+    r = self.Distance(other)
+    Q = other.coulomb
+    _F_ = -k * self.coulomb * Q / r**2
+    displacement = self.Displacement(other)
+    direction = displacement / r
+    F = direction * _F_
     return F
   
+  # Calculate the total net force
+  def NetForce(self, others=None):
+    if others:
+      for other in others:
+        self.netforce += self.GravitationalForce(other) + self.ElectrostaticForce(other)
+
   # Update the position and velocity
   def update(self, others=None, dt=0.1):
-    F = self.NetForce(others)
-    self.acceleration = F / self.mass
+    if self.fixed:
+      return
+    self.NetForce(others)
+    self.acceleration = self.netforce / self.mass
     self.velocity += self.acceleration * dt
     self.coordinate += self.velocity * dt
